306558d8f732d5fd7ab7f8ec183bfd1ce176149f
admica <adrian.m.carr@nasa.gov>
2024-12-16 00:30:14 -0600

    avatar stuff

--- frontend/src/components/UserNav.tsx
+++ frontend/src/components/UserNav.tsx
@@ -32,17 +32,17 @@ export function UserNav() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" className="relative h-8 w-8 rounded-full">
           <Avatar className="h-8 w-8">
-            <AvatarImage src="/avatars/01.png" alt="@username" />
-            <AvatarFallback>U</AvatarFallback>
+            <AvatarFallback>
+              {(localStorage.getItem('username') || 'U')[0].toUpperCase()}
+            </AvatarFallback>
           </Avatar>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
         <DropdownMenuLabel className="font-normal">
           <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">Username</p>
-            <p className="text-xs leading-none text-muted-foreground">
-              user@example.com
+            <p className="text-sm font-medium leading-none">
+              {localStorage.getItem('username') || 'User'}
             </p>
           </div>
         </DropdownMenuLabel>
